9438b4de8796b6bf787a7aca4b51bf8bdd2c083a
MontuuOfficial <michal.kociubinski@wp.pl>
2025-09-15 12:40:03 +0200

    typy proste

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Dzień dobry");
+
+        //typy calkowite
+        byte osiem = 8;
+        short trzysta = 300;
+        int tysiac = 1000;
+        long dziesiecTysiecy = 10000;
+        System.out.println("Twoja liczba " + osiem);
+
+        //typy zmiennoprzecinkowe
+        float liczbaPi = 3.14f; //koniecznie f na koncu
+        double pierwiastekZ2 = 1.41;
+        System.out.println("Liczba pi to " + liczbaPi);
+
+        //typ logiczny
+        boolean prawda = true;
+
+        //typ znakowy
+        char znak = 'a';
+
+
     }
 }
