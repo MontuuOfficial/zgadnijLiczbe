c37db84e482cd6c86b792cda503fbb9654639750
MontuuOfficial <michal.kociubinski@wp.pl>
2025-09-22 13:35:47 +0200

    cwiczenia

--- src/Main.java
+++ src/Main.java
@@ -1,141 +1,45 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Dzień dobry");
-        /*
-        //typy calkowite
-        byte osiem = 8;
-        short trzysta = 300;
-        int tysiac = 1000;
-        long dziesiecTysiecy = 10000;
-        System.out.println("Twoja liczba " + osiem);
-
-        //typy zmiennoprzecinkowe
-        float liczbaPi = 3.14f; //koniecznie f na koncu
-        double pierwiastekZ2 = 1.41;
-        System.out.println("Liczba pi to " + liczbaPi);
-
-        //typ logiczny
-        boolean prawda = true;
-
-        //typ znakowy
-        char znak = 'a';
+        //Wypisz wszystkie liczby dwucyfrowe parzyste
+        //Zapytaj użytkownika ile chce gwiazdek i tyle mu napisz
+        //Poproś użytkownika o 0 i wczytuj liczby tak długo, aż będzie 0
+        //Oblicz sume wszystkich liczb od a do b wczytanych z klawiatury
 
         //Zadanie 1
-        //Napisz program który losuje liczbe od 1 do 100, następnie zapytaj o nią użytkownika
-        int randomNumber = (int)(Math.random() * 100) + 1;
-        System.out.println(randomNumber);
-        //wczytywanie liczby z klawiatury
+        System.out.println("Liczby dwucyfrowe parzyste");
+        for (int i = 10; i < 100; i = i + 2) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        //Zadanie 2
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj liczbe ");
-        int guessedNumber = scanner.nextInt();
-        System.out.println("Podano " + guessedNumber);
-        if (guessedNumber == randomNumber){
-            System.out.println("Ok");
+        System.out.println("Ile gwiazdek wypisać?");
+        int starAmount = scanner.nextInt();
+        for (int j = 0; j < starAmount; j++) {
+            System.out.print("*");
         }
-        else {
-            System.out.println("Nie ok");
-        }
-        int difference;
-        if (randomNumber > guessedNumber) {
-            difference = randomNumber - guessedNumber;
-        }
-        else {
-            difference = guessedNumber - randomNumber;
-        }
-        //wyrazenie warunkowe
-        //difference = guessedNumber > randomNumber? randomNumber - guessedNumber : guessedNumber - randomNumber;
-        difference = difference / 10;
-
-        //instrukcja wyboru
-        switch (difference) {
-            case 0:
-                System.out.println("Bardzo blisko");
-                break;
-            case 1:
-                System.out.println("Dość blisko");
-                break;
-            case 2:
-                System.out.println("Git xd");
-            default:
-                System.out.println("Spróbuj czego innego");
-        }
-        //wyrażenie switch
-        System.out.println(
-                switch (difference){
-                    case 0 -> "Bardzo blisko";
-                    case 1 -> "Dość blisko";
-                    case 2 -> "Git xd";
-                    default -> "Spróbuj czego innego";
-                }
-        );
-        //zgadywanie 10 razy
-        Random random = new Random();
-        randomNumber = random.nextInt(1, 101);
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Zgadnij liczbę");
-            guessedNumber = scanner.nextInt();
-            if (guessedNumber == randomNumber) {
-                System.out.println("Gratki");
-                break;
-            }
-            else if(guessedNumber > randomNumber){
-                System.out.println("Za dużo");
-            }
-            else {
-                System.out.println("Za mało");
-            }
-        }*/
-        //git
-        //git init
-        //git config user.email "email"
-        //git config user.name "name"
-        //git add .
-        //git commit -m "tresc comitu"
-        //git remote add
-        //git push -u origin master
-        //git push
-
-        //za drugim razem
-        //git add .
-        //git commit -m "tresc comitu"
-        //git push
-
-        //zgadnij liczbe, dopóki nie zgadniesz
-        /*Random random = new Random();
-        Scanner scanner = new Scanner(System.in);
-        int randomNumber = random.nextInt(1, 101);
-        System.out.println("Podaj losową liczbę");
-        int guessedNumber = scanner.nextInt();
-        while (randomNumber != guessedNumber) {
-            if (randomNumber < guessedNumber) {
-                System.out.println("Wpisano za dużo");
-            }
-            else {
-                System.out.println("Wpisano za mało");
-            }
-            System.out.println("Podaj losową liczbę");
-            guessedNumber = scanner.nextInt();
-        }
-        */
-        Random random = new Random();
-        Scanner scanner = new Scanner(System.in);
-        int guessedNumber;
-        int randomNumber = random.nextInt(1, 101);
+        System.out.println();
+        //Zadanie 3
+        int scannedValue;
         do {
-            System.out.println("Podaj liczbe");
-            guessedNumber = scanner.nextInt();
-            if (randomNumber != guessedNumber) {
-                if (randomNumber < guessedNumber) {
-                    System.out.println("Wpisano za dużo");
-                }
-                else {
-                    System.out.println("Wpisano za mało");
-                }
-            }
-        }while (randomNumber != guessedNumber);
-    }
-}
+            System.out.println("Podaj zero");
+            scannedValue = scanner.nextInt();
+        }while (scannedValue != 0);
+        System.out.println();
+        //Zadanie 4
+        int usersA, usersB, sumFromAToB = 0;
+        System.out.println("Podaj wartość A");
+        usersA = scanner.nextInt();
+        System.out.println("Podaj wartość B");
+        usersB = scanner.nextInt();
+        for (int k = usersA; k <= usersB; k++) {
+            sumFromAToB = sumFromAToB + k;
+        }
+        System.out.println("Suma wszystkich liczb od A do B to " + sumFromAToB);
 
+        System.out.println();
+    }
+
+}
---
+++ src/Main2.java
@@ -0,0 +1,141 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("Dzień dobry");
+        /*
+        //typy calkowite
+        byte osiem = 8;
+        short trzysta = 300;
+        int tysiac = 1000;
+        long dziesiecTysiecy = 10000;
+        System.out.println("Twoja liczba " + osiem);
+
+        //typy zmiennoprzecinkowe
+        float liczbaPi = 3.14f; //koniecznie f na koncu
+        double pierwiastekZ2 = 1.41;
+        System.out.println("Liczba pi to " + liczbaPi);
+
+        //typ logiczny
+        boolean prawda = true;
+
+        //typ znakowy
+        char znak = 'a';
+
+        //Zadanie 1
+        //Napisz program który losuje liczbe od 1 do 100, następnie zapytaj o nią użytkownika
+        int randomNumber = (int)(Math.random() * 100) + 1;
+        System.out.println(randomNumber);
+        //wczytywanie liczby z klawiatury
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbe ");
+        int guessedNumber = scanner.nextInt();
+        System.out.println("Podano " + guessedNumber);
+        if (guessedNumber == randomNumber){
+            System.out.println("Ok");
+        }
+        else {
+            System.out.println("Nie ok");
+        }
+        int difference;
+        if (randomNumber > guessedNumber) {
+            difference = randomNumber - guessedNumber;
+        }
+        else {
+            difference = guessedNumber - randomNumber;
+        }
+        //wyrazenie warunkowe
+        //difference = guessedNumber > randomNumber? randomNumber - guessedNumber : guessedNumber - randomNumber;
+        difference = difference / 10;
+
+        //instrukcja wyboru
+        switch (difference) {
+            case 0:
+                System.out.println("Bardzo blisko");
+                break;
+            case 1:
+                System.out.println("Dość blisko");
+                break;
+            case 2:
+                System.out.println("Git xd");
+            default:
+                System.out.println("Spróbuj czego innego");
+        }
+        //wyrażenie switch
+        System.out.println(
+                switch (difference){
+                    case 0 -> "Bardzo blisko";
+                    case 1 -> "Dość blisko";
+                    case 2 -> "Git xd";
+                    default -> "Spróbuj czego innego";
+                }
+        );
+        //zgadywanie 10 razy
+        Random random = new Random();
+        randomNumber = random.nextInt(1, 101);
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Zgadnij liczbę");
+            guessedNumber = scanner.nextInt();
+            if (guessedNumber == randomNumber) {
+                System.out.println("Gratki");
+                break;
+            }
+            else if(guessedNumber > randomNumber){
+                System.out.println("Za dużo");
+            }
+            else {
+                System.out.println("Za mało");
+            }
+        }*/
+        //git
+        //git init
+        //git config user.email "email"
+        //git config user.name "name"
+        //git add .
+        //git commit -m "tresc comitu"
+        //git remote add
+        //git push -u origin master
+        //git push
+
+        //za drugim razem
+        //git add .
+        //git commit -m "tresc comitu"
+        //git push
+
+        //zgadnij liczbe, dopóki nie zgadniesz
+        /*Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int randomNumber = random.nextInt(1, 101);
+        System.out.println("Podaj losową liczbę");
+        int guessedNumber = scanner.nextInt();
+        while (randomNumber != guessedNumber) {
+            if (randomNumber < guessedNumber) {
+                System.out.println("Wpisano za dużo");
+            }
+            else {
+                System.out.println("Wpisano za mało");
+            }
+            System.out.println("Podaj losową liczbę");
+            guessedNumber = scanner.nextInt();
+        }
+        */
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int guessedNumber;
+        int randomNumber = random.nextInt(1, 101);
+        do {
+            System.out.println("Podaj liczbe");
+            guessedNumber = scanner.nextInt();
+            if (randomNumber != guessedNumber) {
+                if (randomNumber < guessedNumber) {
+                    System.out.println("Wpisano za dużo");
+                }
+                else {
+                    System.out.println("Wpisano za mało");
+                }
+            }
+        }while (randomNumber != guessedNumber);
+    }
+}
+
